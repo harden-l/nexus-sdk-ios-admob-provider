4476e2d3e7de20889eb51b939d562343d2de29c2
liuyun <liuyun@xuejj.com>
2026-08-03 19:26:34 +0800

    Release AdMob Provider 0.0.6

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "NexusGrowthAnalyticsAdAdMob", targets: ["NexusGrowthAnalyticsAdAdMob"])
     ],
     dependencies: [
-        .package(url: "https://github.com/harden-l/nexus-sdk-ios.git", exact: "0.0.5"),
+        .package(url: "https://github.com/harden-l/nexus-sdk-ios.git", exact: "0.0.6"),
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "12.0.0")
     ],
     targets: [
